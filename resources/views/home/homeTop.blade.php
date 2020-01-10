f957ec6b72a0364353f070ed5ab9873e2a611f97
customaddone <custom1485@docomo.ne.jp>
2020-01-10 13:05:37 +0000

    make homeTop #2

--- resources/views/home/homeTop.blade.php
+++ resources/views/home/homeTop.blade.php
@@ -5,20 +5,32 @@
     @endcomponent
 
     <!--  まずは適当にボックスを並べる -->
-    <div class="main-image">
-    </div>
+    <main>
+        <div class="row">
+            <div class="main-image">
+                あ
+            </div>
 
-    <div class="content">
-    </div>
+            <div class="content">
+                あ
+            </div>
 
-    <div class="content">
-    </div>
+            <div class="content">
+                あ
+            </div>
 
-    <div class="content">
-    </div>
+            <div class="content">
+                あ
+            </div>
 
-    <div class="content">
-    </div>
+            <div class="content-footer">
+                あ
+            </div>
+        </div>
+    </main>
+
+    <!-- 他のscssはheader,footerでくくっているので下記のcssとコンフリクトしない -->
+    <script src="/js/homeTop.js" type="text/javascript"></script>
 
     @component('components.footer')
     @endcomponent
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('home.show');
+    return view('home.homeTop');
 });
