0cfbd5ee36f4221a2615cfab355cc37c4bc918d4
customfolk <custom1485@gmail.com>
2020-01-16 00:13:11 +0900

    RDS #2

--- terraform/rds/aws_db_module/aws_db_module_file.tf
+++ terraform/rds/aws_db_module/aws_db_module_file.tf
@@ -1,89 +1,34 @@
-variable "name" {
-  type = "string"
+variable "name" {}
+variable "vpc_id" {}
+variable "port" {}
+
+variable "cidr_blocks" {
+  type = list(string)
 }
 
-variable "vpc_id" {
-  type = "string"
+resource "aws_security_group" "default" {
+  name   = var.name
+  vpc_id = var.vpc_id
 }
 
-variable "subnet_ids" {
-  type = "list"
+resource "aws_security_group_rule" "ingress" {
+  type              = "ingress"
+  from_port         = var.port
+  to_port           = var.port
+  protocol          = "tcp"
+  cidr_blocks       = var.cidr_blocks
+  security_group_id = aws_security_group.default.id
 }
 
-variable "database_name" {
-  type = "string"
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.default.id
 }
 
-variable "master_username" {
-  type = "string"
-}
-
-variable "master_password" {
-  type = "string"
-}
-
-locals {
-  name = "${var.name}-mysql"
-}
-
-resource "aws_security_group" "this" {
-  name        = "${local.name}"
-  description = "${local.name}"
-
-  vpc_id = "${var.vpc_id}"
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${local.name}"
-  }
-}
-
-resource "aws_security_group_rule" "mysql" {
-  security_group_id = "${aws_security_group.this.id}"
-
-  type = "ingress"
-
-  from_port   = 3306
-  to_port     = 3306
-  protocol    = "tcp"
-  cidr_blocks = ["10.0.0.0/16"]
-}
-
-resource "aws_db_subnet_group" "this" {
-  name        = "${local.name}"
-  description = "${local.name}"
-  subnet_ids  = "${var.subnet_ids}"
-}
-
-resource "aws_rds_cluster" "this" {
-  cluster_identifier = "${local.name}"
-
-  db_subnet_group_name   = "${aws_db_subnet_group.this.name}"
-  vpc_security_group_ids = ["${aws_security_group.this.id}"]
-
-  engine = "aurora-mysql"
-  port   = "3306"
-
-  database_name   = "${var.database_name}"
-  master_username = "${var.master_username}"
-  master_password = "${var.master_password}"
-}
-
-resource "aws_rds_cluster_instance" "this" {
-  identifier         = "${local.name}"
-  cluster_identifier = "${aws_rds_cluster.this.id}"
-
-  engine = "aurora-mysql"
-
-  instance_class = "db.t3.small"
-}
-
-output "endpoint" {
-  value = "${aws_rds_cluster.this.endpoint}"
+output "security_group_id" {
+  value = aws_security_group.default.id
 }
