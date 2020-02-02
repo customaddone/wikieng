95ddaf79a2bafc213c1aca9221f5620c727642e0
customfolk <custom1485@gmail.com>
2020-02-02 21:40:41 +0900

    ArticlesCotroller Test

--- resources/js/showArticleDetail.js
+++ resources/js/showArticleDetail.js
@@ -53,7 +53,7 @@ var vm = new Vue({
                   var keyId = Object.keys(response.data.query.pages)
                   this.summary = response.data.query.pages[keyId].extract
                                      .replace(/<.+?>/g, "")
-                                     .slice(0, 160);
+                                     .slice(0, 200);
              })
         .catch(response => console.log(response));
     },
--- tests/Feature/ArticlesTest.php
+++ tests/Feature/ArticlesTest.php
@@ -2,25 +2,17 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class HelloTest extends TestCase
+class ArticlesTest extends TestCase
 {
-    // use DatabaseTransactions;でテストの際データベースを初期化
-    use DatabaseTransactions;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-    
     public function testExample()
     {
         $response = $this->get('/');
