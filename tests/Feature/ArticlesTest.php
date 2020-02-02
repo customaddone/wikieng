2aab585d2dfad6052bb8684ab778091660159150
customfolk <custom1485@gmail.com>
2020-02-03 07:23:58 +0900

    Test article function

--- tests/Feature/ArticlesTest.php
+++ tests/Feature/ArticlesTest.php
@@ -6,7 +6,10 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Http\Request;
 use App\User;
+use App\Article;
+use App\Http\Controllers\ArticlesController;
 use Illuminate\Support\Facades\Auth;
 
 class ArticlesTest extends TestCase
@@ -27,6 +30,8 @@ class ArticlesTest extends TestCase
 
     public function testArticle()
     {
+        $article = new ArticlesController;
+
         // ログイン用ユーザー
         $user = factory(User::class)->create();
         // 作ったユーザーで認証します
@@ -34,8 +39,20 @@ class ArticlesTest extends TestCase
         // 認証されているユーザーがいればidがあるか調べます
         $this->assertNotEmpty(Auth::id());
 
-        $response = $this->get('/');
+        // requestの作成
+        $request = new Request;
+        $request->merge([
+            'title' => 'article',
+            'user_id' => Auth::id(),
+            'article' => 'article_body',
+            'summary' => 'article_summary',
+            'status' => 'status'
+        ]);
 
-        $response->assertStatus(200);
+        // importには戻り値はないので、import($request)自体はnullになる
+        $article->import($request);
+        // ちゃんとarticleが生成されるか？
+        $this->assertNotEmpty(Article::where('title', '=', 'article'));
+
     }
 }
