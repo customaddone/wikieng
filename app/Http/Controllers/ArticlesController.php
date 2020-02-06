8a965007f554e6aa6184e4893156c05ad841aab8
customfolk <custom1485@gmail.com>
2020-02-06 22:45:35 +0900

    static analysis level5

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -17,9 +17,9 @@ class ArticlesController extends Controller
     public function index() {
         // idとタイトルだけgetする
         // idの前にはテーブルの名前をつける
-       $articles = Article::where('user_id', '=', Auth::id())->select('id', 'title', 'summary')
-           ->get();
-       return view('articles.myArticles', [ 'articles' => $articles ]);
+        $articles = Article::where('user_id', '=', Auth::id())->select('id', 'title', 'summary')
+            ->get();
+        return view('articles.myArticles', [ 'articles' => $articles ]);
     }
 
     public function show($id) {
